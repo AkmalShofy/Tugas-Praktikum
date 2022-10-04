38d626c3167745f537a96eb668baab9f7b06d4df
AkmalShofy <114986742+AkmalShofy@users.noreply.github.com>
2022-10-04 17:56:08 +0800

    Add files via upload

---
+++ tugas/PRAK002-2210817210023-Muhammad Shofy Akmal.py
@@ -0,0 +1,3 @@
+print ("Selamat Pagi, Muhammad Shofy Akmal")
+print ("Selamat Siang, Muhammad Shofy Akmal")
+print ("Selamat Malam, Muhammad Shofy Akmal")
---
+++ tugas/PRAK002-2210817210023-MuhammadShofyAkmal.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main () {
+printf("Selamat Pagi, Muhammad Shofy Akmal\n");
+printf("Selamat Siang, Muhammad Shofy Akmal\n");
+printf("Selamat Malam, Muhammad Shofy Akmal\n");
+}
