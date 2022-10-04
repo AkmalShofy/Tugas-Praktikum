e0a42d6cc31107781bfb974dcced69a721d4ad3e
akmalshofy <shofyakmal@gmail.com>
2022-10-04 16:58:43 +0800

    Tugas 1

---
+++ tugas/PRAK004-2210817210023-MuhammadShofyAkmal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {  
+printf(" ############################    \n");
+printf(" #                          #    \n");
+printf(" #   Muhammad Shofy Akmal   #    \n");
+printf(" #      2210817210023       #    \n");
+printf(" #                          #    \n");
+printf(" ############################    \n");
+}
